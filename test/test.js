7a8b3c0356a79a2628b8d7a053d2e77e44413719
rockywu 吴佳雷 <rockywu@anjuke.com, wjl19890427@hotmail.com>
2015-08-03 14:26:03 +0800

    add

---
+++ test/test.js
@@ -0,0 +1,25 @@
+
+/*
+ * 分页函数
+ */
+var pagination = Tools.pagination = function(options) {
+    var def = {
+        page_num : 1,
+        page_size : 10
+    };
+
+}
+
+/*
+ * 上一页
+ */
+pagination.prototype.next = function() {
+
+}
+
+/*
+ * 下一页
+ */
+pagination.prototype.previous = function() {
+
+}
